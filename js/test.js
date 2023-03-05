512fbfe8b073164d91f95aef63e722eca69481ce
MiroslavAndric <mixxx123@gmail.com>
2023-03-05 17:28:16 +0100

    'Poruka'

--- js/test.js
+++ js/test.js
@@ -11,18 +11,46 @@
 // console.log(http)
 
 
-const path = require("path");
+// const path = require("path");
 
-const testPath = "C:\Users\mixxx\Desktop\CODE Predavanja\29\cas29\class36\js\test.txt"
+// const testPath = "C:\Users\mixxx\Desktop\CODE Predavanja\29\cas29\class36\js\test.txt"
 
-const baseName = path.basename(testPath);
-console.log(baseName)
+// const baseName = path.basename(testPath);
+// console.log(baseName)
 
-const extension = path.extname(testPath)
-console.log(extension);
+// const extension = path.extname(testPath)
+// console.log(extension);
 
-const location = path.dirname(testPath);
-console.log(location);
+// const location = path.dirname(testPath);
+// console.log(location);
 
-const pathInfo = path.parse(testPath);
-console.log(pathInfo.);
+// const pathInfo = path.parse(testPath);
+// // console.log(pathInfo.);
+
+// const os = require ("os");
+
+// console.log(os.platform());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+// console.log(os.arch());
+// console.log(os.cpus())
+// console.log(os.endianness())
+
+const fs = require("fs");
+fs.readFile("test.txt", "utf-8", (err, data) => {
+    if(err) {
+        throw err;
+    }
+    console.log('sadrzaj: ')
+    console.log(data)
+});
+
+const sadrzaj = 'Neki novi sadrzaj koji ce se upisati';
+
+fs.writeFile("test.txt", sadrzaj, () => {
+    if(err){
+        throw console.error;
+    }
+    console.log('ispisan je novi sadrzaj')
+})
+ 
